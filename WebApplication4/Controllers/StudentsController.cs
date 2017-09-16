9968d49895e54c1d6e7d89e69f08dcfcd953caa5
ahmedrazarafaqat <ahmedrazarafaqat@gmail.com>
2017-09-16 22:57:27 +0500

    Commit1

---
+++ WebApplication4/Controllers/StudentsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private AttendanceEntities db = new AttendanceEntities();
+        // GET: Students
+        public ActionResult Index()
+        {
+            var batch_name = db.Batches.ToList();
+            return View(batch_name);
+        }
+
+
+        [HttpGet]
+        public ActionResult Student(int drop)
+        {
+
+            var db = new AttendanceEntities();
+            var batch = db.Students.Where( s=> s.Batch_Id == drop);
+            return PartialView(batch);
+        }
+
+        // GET: Students1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students1/Create
+        public ActionResult Create()
+        {
+            ViewBag.Batch_Id = new SelectList(db.Batches, "Id", "Name");
+            return View();
+        }
+
+        // POST: Students1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Batch_Id")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Batch_Id = new SelectList(db.Batches, "Id", "Name", student.Batch_Id);
+            return View(student);
+        }
+
+        // GET: Students1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Batch_Id = new SelectList(db.Batches, "Id", "Name", student.Batch_Id);
+            return View(student);
+        }
+
+        // POST: Students1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Batch_Id")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Batch_Id = new SelectList(db.Batches, "Id", "Name", student.Batch_Id);
+            return View(student);
+        }
+
+        // GET: Students1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
